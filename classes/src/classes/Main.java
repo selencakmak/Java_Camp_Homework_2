036ffdb02f31c1d49856364edad9e42e93295020
selencakmak <76444550+selencakmak@users.noreply.github.com>
2022-09-28 12:17:39 +0300

    Java Camp

--- classes/src/classes/Main.java
+++ classes/src/classes/Main.java
@@ -12,7 +12,21 @@ public class Main {
 		customerManager.remove();
 		customerManager.update();
 		
-
+		//value
+		int sayi1=10;
+		int sayi2=20;
+		sayi2=sayi1;
+		sayi1=30;
+		System.out.println(sayi2);
+		
+		//diziler referans tiptir.
+		
+		int[] sayilar1 = new int[] {1,2,3};
+		int[] sayilar2 = new int[] {4,5,6};
+		sayilar2=sayilar1;
+		sayilar1[0]=10;
+		System.out.println(sayilar2[0]);
+		
 	}
 
 }
